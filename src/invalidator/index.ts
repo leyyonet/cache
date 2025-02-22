68c7b8bf88bd0185458c3236ceba9654ae6a1396
Mustafa Yelmer <mustafayelmer@gmail.com>
2025-02-23 02:49:58 +0300

    export set abstract

--- src/invalidator/index.ts
+++ src/invalidator/index.ts
@@ -1,4 +1,4 @@
-export * from './types';
-export * from './result-invalidator';
 export * from './dummy-invalidator';
 export * from './prop-invalidator-abstract';
+export * from './result-invalidator';
+export * from './types';
