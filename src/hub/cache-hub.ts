01492d671c14ecbfeee16ac93c115d7dc6ab8840
Mustafa Yelmer <mustafayelmer@gmail.com>
2025-02-23 03:56:10 +0300

    create client, get client, list channels, list providers

--- src/hub/cache-hub.ts
+++ src/hub/cache-hub.ts
@@ -1,24 +1,78 @@
-import {CacheChannelCreatorLambda, CacheHub} from "./types";
+import {CacheChannelCreatorLambda, CacheClientResult, CacheHub, CacheHubChannelItem, CacheHubClientRepo} from "./types";
 import {CacheChannel, CacheID, TR} from "../channel";
-import {CachePropCompleted} from "../prop";
-import {CacheClientLike} from "../client";
+import {CachePropChannel, CachePropCompleted} from "../prop";
+import {CacheClient, CacheClientLike} from "../client";
 
 class CacheHubImpl implements CacheHub{
-    private _channelCreators: Map<string, CacheChannelCreatorLambda<TR, string, unknown>>;
+    private _providers: Map<string, CacheChannelCreatorLambda<TR, string, unknown>>;
+    private _clients: Map<Object, CacheHubClientRepo<TR, string, unknown>>;
 
     constructor() {
-        this._channelCreators = new Map();
+        this._providers = new Map();
+        this._clients = new Map();
     }
 
+    protected _getClient<A extends TR, N extends CacheID, C>(native: C, provider: string): CacheClientResult<A, N, C> {
+        if (!provider) {
+            throw new Error(`Empty client provider`);
+        }
+        if (!native) {
+            throw new Error(`Empty client native connection for ${provider}`);
+        }
+        const lambda = this._providers.get(provider);
+        if (!lambda) {
+            const providers = this.listProviders();
+            if (providers.length < 1) {
+                throw new Error(`Invalid provider for ${provider}, there is not any available provider`);
+            }
+            throw new Error(`Invalid provider for ${provider}, available providers: ${providers}`);
+        }
+        let client: CacheClientLike<C>;
+        let repo = this._clients.get(native);
+        if (!repo) {
+            client = new CacheClient<C>(native, provider);
+            repo = {client, channels: []};
+            this._clients.set(native, repo);
+        }
+        return {lambda, repo} as CacheClientResult<A, N, C>;
+    }
+
+    listProviders(): Array<string> {
+        return Array.from(this._providers.keys());
+    }
+    listChannels(): Array<CacheHubChannelItem<TR>> {
+        const arr = [] as Array<CacheHubChannelItem<TR>>;
+        Array.from(this._clients.values()).forEach(repo => {
+            const provider = repo.client.provider;
+            repo.channels.forEach(channel => {
+                const prop = channel.prop as CachePropChannel<TR>;
+                arr.push({provider, ...prop});
+            });
+        });
+        return arr;
+    }
+
+    getClient<C>(native: C, provider: string): CacheClientLike<C> {
+        return this._getClient<TR, CacheID, C>(native, provider).repo.client;
+    }
     setChannelCreator<A extends TR = TR, N extends CacheID = string, C = unknown>(provider: string, lambda: CacheChannelCreatorLambda<A, N, C>): void {
-        this._channelCreators.set(provider, lambda as CacheChannelCreatorLambda<TR, string, unknown>);
+        if (this._providers.has(provider)) {
+            throw new Error(`Duplicated provider for ${provider}`);
+        }
+        this._providers.set(provider, lambda as CacheChannelCreatorLambda<TR, string, unknown>);
     }
     createChannel<A extends TR, N extends CacheID = string, C = unknown>(client: CacheClientLike<C>, prop: CachePropCompleted<A>): CacheChannel<A, N, C> {
-        const lambda = this._channelCreators.get(client?.provider);
-        if (!lambda) {
-            throw new Error('Invalid provider'); // todo
+        if (!client) {
+            throw new Error(`Empty client wrapper`);
         }
-        return lambda(client, prop as CachePropCompleted<TR>) as CacheChannel<A, N, C>;
+        const result = this._getClient<TR, CacheID, C>(client.native, client.provider);
+        const index = result.repo.channels.findIndex(channel => channel.prop.prefix === prop.prefix);
+        if (index >= 0) {
+            console.warn(`Same prefix was used! ==> ${prop.prefix}`);
+        }
+        const channel = result.lambda(client, prop as CachePropCompleted<TR>) as CacheChannel<A, N, C>;
+        result.repo.channels.push(channel as CacheChannel<TR, CacheID, C>);
+        return channel;
     }
 }
 export const cacheHub: CacheHub = new CacheHubImpl();
--- src/hub/types.ts
+++ src/hub/types.ts
@@ -1,9 +1,24 @@
 import {CacheChannel, CacheID, TR} from "../channel";
 import {CacheClientLike} from "../client";
-import {CachePropCompleted} from "../prop";
+import {CachePropChannel, CachePropCompleted, CachePropReadonly} from "../prop";
 
 export type CacheChannelCreatorLambda<A extends TR, N extends CacheID, C> = (client: CacheClientLike<C>, prop: CachePropCompleted<A>) => CacheChannel<A, N, C>;
 export interface CacheHub {
+    listProviders(): Array<string>;
+    listChannels(): Array<CacheHubChannelItem<TR>>;
+    getClient<C>(native: C, provider: string): CacheClientLike<C>;
     setChannelCreator<A extends TR = TR, N extends CacheID = string, C = unknown>(provider: string, lambda: CacheChannelCreatorLambda<A, N, C>): void;
     createChannel<A extends TR = TR, N extends CacheID = string, C = unknown>(client: CacheClientLike<C>, prop: CachePropCompleted<A>): CacheChannel<A, N, C>;
+}
+
+export interface CacheHubClientRepo<A extends TR, N extends CacheID, C> {
+    client: CacheClientLike<C>;
+    channels: Array<CacheChannel<A, N, C>>;
+}
+export interface CacheHubChannelItem<A extends TR> extends CachePropChannel<A> {
+    provider: string;
+}
+export interface CacheClientResult<A extends TR, N extends CacheID, C> {
+    lambda: CacheChannelCreatorLambda<A, N, C>;
+    repo: CacheHubClientRepo<A, N, C>;
 }
